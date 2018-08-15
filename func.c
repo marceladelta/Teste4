0cdba2835090a89b65ec063f381856e2b0238ac7
Marcela <marcela-reis0111@hotmail.com>
2018-08-15 16:08:53 -0300

    Adicionado funcionalidade de soma.

---
+++ func.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+  
+  int main(){
+    int soma(int a, int b){
+     return a + b;
+  
+  }
+
--- hello1.c
+++ hello1.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
   
   int main(){
-    printf("Hello World!");
+    int ret;
+    ret = soma(2, 3);
+    printf("O resultado é: %d", ret);
     return 0;
 
   }
